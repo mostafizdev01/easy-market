e03993f432e3e6f77ed74fbf8d49bf4723c71ed6
Hera <mostafizhera@gmail.com>
2025-03-07 02:13:52 +0600

    showing the job detaile all text

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -1,10 +1,56 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { format } from 'date-fns'
+import { useContext, useEffect, useState } from 'react'
 
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
+import { useParams } from 'react-router-dom'
+import { AuthContext } from '../providers/AuthProvider'
 
 const JobDetails = () => {
   const [startDate, setStartDate] = useState(new Date())
+  const { id } = useParams();
+  const [job, setJob] = useState({});
+  const  { user } = useContext(AuthContext);
+  
+  
+  const { _id, job_title, job_description, deadline, maxPrice, minPrice, category, bids, employeInfo } = job;
+
+  useEffect(() => {
+    fetchSingleJobs()  // Call your API here to fetch data related to the job details.
+  }, [id])
+
+  const fetchSingleJobs = async () => {
+    const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/jobs/${id}`);
+    setJob(data);
+  }
+
+  const deadlineDate = job?.deadline ? format(new Date(job.deadline), "dd/MM/yyyy") : "No Deadline";
+
+  /// post the jobd job from the database 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const from = e.target;
+    const price = from.price.value;
+    const comment = from.comment.value;
+    // const deadline = from.startDate;
+    
+    const bidInfo = {
+      price,
+      email: user?.email,
+      comment,
+      deadline: startDate
+    }
+
+    /// post the data in the database 
+    
+    const {data} = await axios.post(`${import.meta.env.VITE_API_URL}/bid-job`, bidInfo);
+    if (data) {
+      alert("Your bids was successfully")
+    }
+    
+  }
 
   return (
     <div className='flex flex-col md:flex-row justify-around gap-5  items-center min-h-[calc(100vh-306px)] md:max-w-screen-xl mx-auto '>
@@ -12,22 +58,20 @@ const JobDetails = () => {
       <div className='flex-1  px-4 py-7 bg-white rounded-md shadow-md md:min-h-[350px]'>
         <div className='flex items-center justify-between'>
           <span className='text-sm font-light text-gray-800 '>
-            Deadline: 28/05/2024
+            Deadline: {deadlineDate}
           </span>
           <span className='px-4 py-1 text-xs text-blue-800 uppercase bg-blue-200 rounded-full '>
-            Web Development
+            {category}
           </span>
         </div>
 
         <div>
           <h1 className='mt-2 text-3xl font-semibold text-gray-800 '>
-            Web Development
+            {job_title}
           </h1>
 
           <p className='mt-2 text-lg text-gray-600 '>
-            Dramatically redefine bleeding-edge infrastructures after
-            client-focused value. Intrinsicly seize user-centric partnerships
-            through out-of-the-box architectures. Distinctively.
+            {job_description}
           </p>
           <p className='mt-6 text-sm font-bold text-gray-600 '>
             Buyer Details:
@@ -35,31 +79,33 @@ const JobDetails = () => {
           <div className='flex items-center gap-5'>
             <div>
               <p className='mt-2 text-sm  text-gray-600 '>
-                Name: Programming-Hero Instructors
+                Name: {employeInfo?.name}
               </p>
               <p className='mt-2 text-sm  text-gray-600 '>
-                Email: instructors@programming-hero.com
+              {employeInfo?.email}
               </p>
             </div>
             <div className='rounded-full object-cover overflow-hidden w-14 h-14'>
               <img
-                src='https://i.ibb.co.com/qsfs2TW/Ix-I18-R8-Y-400x400.jpg'
+                src={employeInfo?.photo}
                 alt=''
               />
             </div>
           </div>
           <p className='mt-6 text-lg font-bold text-gray-600 '>
-            Range: $500 - $600
+            Range: ${minPrice} - ${maxPrice}
           </p>
         </div>
       </div>
+
       {/* Place A Bid Form */}
+
       <section className='p-6 w-full  bg-white rounded-md shadow-md flex-1 md:min-h-[350px]'>
         <h2 className='text-lg font-semibold text-gray-700 capitalize '>
           Place A Bid
         </h2>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
             <div>
               <label className='text-gray-700 ' htmlFor='price'>
@@ -82,6 +128,7 @@ const JobDetails = () => {
                 id='emailAddress'
                 type='email'
                 name='email'
+                defaultValue={user?.email}
                 disabled
                 className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md   focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring'
               />
